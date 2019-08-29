ebe470877081a4cafc3048e46cb418f525f21cce
Fabian Deitelhoff <Fabian@FabianDeitelhoff.de>
2019-08-29 10:32:47 +0200

    First version of post metadata update (not working atm).

--- posts-from-hashtags.js
+++ posts-from-hashtags.js
@@ -8,182 +8,196 @@ var db = new sqlite3.Database("./data/db.instagram");
 
 let i = 1;
 
-const start = async function () {
-    const hashtags = ["sokofuture"]; // "ac2106"
+const start = async function() {
+  const hashtags = ["sokofuture"]; // "ac2106"
 
-    for (const hashtag of hashtags) {
-        console.log(`Getting data for hashtag ${hashtag}...`);
-        
-        await getHashtagData(hashtag);
-    }    
+  for (const hashtag of hashtags) {
+    console.log(`Getting data for hashtag ${hashtag}...`);
 
-    async function getHashtagData(hashtag = '', hasNextPage = false, endCursor = "") {
-        try {
-            let postsUrl = "";
-            if (!hasNextPage) {
-                postsUrl = `https://www.instagram.com/graphql/query/?query_hash=f92f56d47dc7a55b606908374b43a314&variables={"tag_name":"${hashtag}","first":50}`;
-            } else {
-                postsUrl = `https://www.instagram.com/graphql/query/?query_hash=f92f56d47dc7a55b606908374b43a314&variables={"tag_name":"${hashtag}","first":50,"after":"${endCursor}"}`;
-            }
+    await getHashtagData(hashtag);
+  }
 
-            const response = await axios.get(postsUrl);
-            const edges = response.data.data.hashtag.edge_hashtag_to_media.edges;
-            const pageInfo = response.data.data.hashtag.edge_hashtag_to_media.page_info;
-            
-            for (const post of edges) {
-                if (i % 200 === 0) {
-                    const sleeping = 310;
-                    sleep.sleep(sleeping);
-                    console.log(`Sleeping for ${sleeping} seconds...`);
-                    i = 0;
-                }
+  async function getHashtagData(
+    hashtag = "",
+    hasNextPage = false,
+    endCursor = ""
+  ) {
+    try {
+      let postsUrl = "";
+      if (!hasNextPage) {
+        postsUrl = `https://www.instagram.com/graphql/query/?query_hash=f92f56d47dc7a55b606908374b43a314&variables={"tag_name":"${hashtag}","first":50}`;
+      } else {
+        postsUrl = `https://www.instagram.com/graphql/query/?query_hash=f92f56d47dc7a55b606908374b43a314&variables={"tag_name":"${hashtag}","first":50,"after":"${endCursor}"}`;
+      }
 
-                const node = post.node;
+      const response = await axios.get(postsUrl);
+      const edges = response.data.data.hashtag.edge_hashtag_to_media.edges;
+      const pageInfo =
+        response.data.data.hashtag.edge_hashtag_to_media.page_info;
 
-                console.log(`${i}. Getting data for post ${node.shortcode} for hashtag ${hashtag}...`);
-
-                const caption = node.edge_media_to_caption; // FD: New approach for the "first comment".
-
-                // FD: We use 'username' and 'full_name'.
-                const userData = await getUserData(node.owner.id);
-                if (!userData.username) {
-                    console.log(userData);
-                    return;
-                }
-
-                const postInfo = {
-                    shortcode: node.shortcode,
-                    userid: node.owner.id,
-                    username: userData.username,
-                    userFullName: userData.full_name,
-                    url: `https://www.instagram.com/p/${node.shortcode}`,
-                    caption: caption.edges[0] !== undefined ? caption.edges[0].node.text : '',
-                    imageUrl: node.thumbnail_src,
-                    likesCount: node.edge_media_preview_like.count,
-                    timestamp: node.taken_at_timestamp
-                };
-
-                var dateTime = moment(postInfo.timestamp);
-
-                db.get(
-                    `SELECT Count(1) as PostExists FROM Posts WHERE Shortcode = "${postInfo.shortcode}"`,
-                    async (err, results) => {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            if (results.PostExists) {
-                                await updatePost(hashtag, postInfo.shortcode);
-                            } else {
-                                await createNewPost(hashtag, postInfo, dateTime);
-                            }
-                        }
-                    }
-                );
-
-                i++;
-            }
-
-            if (pageInfo.has_next_page) {
-                await getHashtagData(hashtag, true, pageInfo.end_cursor);
-            }
-        } catch (error) {
-            console.log(error);
+      for (const post of edges) {
+        if (i % 200 === 0) {
+          const sleeping = 310;
+          console.log(`Sleeping for ${sleeping} seconds...`);
+          sleep.sleep(sleeping);
+          i = 0;
         }
-    };
 
-    async function updatePost(hashtag, shortcode) {
-        console.log(`Updating post for shortcode ${shortcode} with hashtag ${hashtag}...`);
+        const node = post.node;
 
-        db.run(
-            `UPDATE Posts SET HashtagSearch = "${hashtag}" WHERE Shortcode = "${shortcode}"`
+        console.log(
+          `${i}. Getting data for post ${
+            node.shortcode
+          } for hashtag ${hashtag}...`
         );
-    }
 
-    async function createNewPost(hashtag, postInfo, dateTime) {
-        console.log(`Saving new post for shortcode ${postInfo.shortcode}...`);
+        const caption = node.edge_media_to_caption; // FD: New approach for the "first comment".
 
-        db.run(
-            "INSERT INTO Posts (ShortCode, UserId, Username, UserFullName, Url, ImageUrl, Caption, LikesCount, Created, CreatedDate, CreatedTime, HashtagSearch, Complete, Error, Chunk) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            [
-                postInfo.shortcode,
-                postInfo.userid,
-                postInfo.username,
-                postInfo.userFullName,
-                postInfo.url,
-                postInfo.imageUrl,
-                postInfo.caption,
-                postInfo.likesCount,
-                postInfo.timestamp,
-                dateTime.format("YYYY-MM-DD"),
-                dateTime.format("HH:mm:ss"),
-                hashtag,
-                0,
-                null,
-                "J3"
-            ]
-        );        
-    }
-
-    async function getUserData(userId = -1) {
-        let userData = {};
-
-        try {
-            const checkUserUrl = `https://www.instagram.com/graphql/query/?query_hash=aec5501414615eca36a9acf075655b1e&variables={"user_id":"${userId}","include_chaining":true,"include_reel":true,"include_suggested_users":false,"include_logged_out_extras":false,"include_highlight_reels":false}`;
-            const checkUserResponse = await axios.get(checkUserUrl, {
-                //   proxy: {
-                //     host: proxy.host,
-                //     port: proxy.port,
-                //     auth: {
-                //       username: proxy.user,
-                //       password: proxy.password
-                //     }
-                //   }
-            });
-
-            const userDataCheck = checkUserResponse.data.data.user.reel.user;
-
-            const url = `https://www.instagram.com/${userDataCheck.username}`;
-            const response = await axios.get(url, {
-                //   proxy: {
-                //     host: proxy.host,
-                //     port: proxy.port,
-                //     auth: {
-                //       username: proxy.user,
-                //       password: proxy.password
-                //     }
-                //   }
-            });
-
-            const root = HTMLParser.parse(response.data, {
-                lowerCaseTagName: false, // convert tag name to lower case (hurt performance heavily)
-                script: true, // retrieve content in <script> (hurt performance slightly)
-                style: false, // retrieve content in <style> (hurt performance slightly)
-                pre: false // retrieve content in <pre> (hurt performance slightly)
-            });
-
-            let scriptData = "";
-            const scriptTags = root.querySelectorAll("script");
-            for (const scriptTag of scriptTags) {
-                if (
-                    typeof scriptTag.rawText.indexOf === "function" &&
-                    scriptTag.rawText.indexOf("window._sharedData = ") === 0
-                ) {
-                    scriptData = scriptTag.rawText.replace(/;/g, "");
-                    scriptData = scriptData.replace(/window._sharedData =/g, "");
-                }
-            }
-
-            const data = JSON.parse(scriptData);
-            userData = data.entry_data.ProfilePage[0].graphql.user;
-            userData.username = userDataCheck.username; // FD: Overwriting the "UsernameChange" with the data from the previous check. The usernames can differ!
-            userData.Error = null;
-        } catch (error) {
-            userData.username = 'Not available';
-            userData.full_name = 'Not available';
+        // FD: We use 'username' and 'full_name'.
+        const userData = await getUserData(node.owner.id);
+        if (!userData.username) {
+          console.log(userData);
+          return;
         }
 
-        return userData;
+        const postInfo = {
+          shortcode: node.shortcode,
+          userid: node.owner.id,
+          username: userData.username,
+          userFullName: userData.full_name,
+          url: `https://www.instagram.com/p/${node.shortcode}`,
+          caption:
+            caption.edges[0] !== undefined ? caption.edges[0].node.text : "",
+          imageUrl: node.thumbnail_src,
+          likesCount: node.edge_media_preview_like.count,
+          timestamp: node.taken_at_timestamp
+        };
+
+        var dateTime = moment(postInfo.timestamp);
+
+        db.get(
+          `SELECT Count(1) as PostExists FROM Posts WHERE Shortcode = "${
+            postInfo.shortcode
+          }"`,
+          async (err, results) => {
+            if (err) {
+              console.log(err);
+            } else {
+              if (results.PostExists) {
+                await updatePost(hashtag, postInfo.shortcode);
+              } else {
+                await createNewPost(hashtag, postInfo, dateTime);
+              }
+            }
+          }
+        );
+
+        i++;
+      }
+
+      if (pageInfo.has_next_page) {
+        await getHashtagData(hashtag, true, pageInfo.end_cursor);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  async function updatePost(hashtag, shortcode) {
+    console.log(
+      `Updating post for shortcode ${shortcode} with hashtag ${hashtag}...`
+    );
+
+    db.run(
+      `UPDATE Posts SET HashtagSearch = "${hashtag}", Chunk = 'J3' WHERE Shortcode = "${shortcode}"`
+    );
+  }
+
+  async function createNewPost(hashtag, postInfo, dateTime) {
+    console.log(`Saving new post for shortcode ${postInfo.shortcode}...`);
+
+    db.run(
+      "INSERT INTO Posts (ShortCode, UserId, Username, UserFullName, Url, ImageUrl, Caption, LikesCount, Created, CreatedDate, CreatedTime, HashtagSearch, Complete, Error, Chunk) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        postInfo.shortcode,
+        postInfo.userid,
+        postInfo.username,
+        postInfo.userFullName,
+        postInfo.url,
+        postInfo.imageUrl,
+        postInfo.caption,
+        postInfo.likesCount,
+        postInfo.timestamp,
+        dateTime.format("YYYY-MM-DD"),
+        dateTime.format("HH:mm:ss"),
+        hashtag,
+        0,
+        null,
+        "J3"
+      ]
+    );
+  }
+
+  async function getUserData(userId = -1) {
+    let userData = {};
+
+    try {
+      const checkUserUrl = `https://www.instagram.com/graphql/query/?query_hash=aec5501414615eca36a9acf075655b1e&variables={"user_id":"${userId}","include_chaining":true,"include_reel":true,"include_suggested_users":false,"include_logged_out_extras":false,"include_highlight_reels":false}`;
+      const checkUserResponse = await axios.get(checkUserUrl, {
+        //   proxy: {
+        //     host: proxy.host,
+        //     port: proxy.port,
+        //     auth: {
+        //       username: proxy.user,
+        //       password: proxy.password
+        //     }
+        //   }
+      });
+
+      const userDataCheck = checkUserResponse.data.data.user.reel.user;
+
+      const url = `https://www.instagram.com/${userDataCheck.username}`;
+      const response = await axios.get(url, {
+        //   proxy: {
+        //     host: proxy.host,
+        //     port: proxy.port,
+        //     auth: {
+        //       username: proxy.user,
+        //       password: proxy.password
+        //     }
+        //   }
+      });
+
+      const root = HTMLParser.parse(response.data, {
+        lowerCaseTagName: false, // convert tag name to lower case (hurt performance heavily)
+        script: true, // retrieve content in <script> (hurt performance slightly)
+        style: false, // retrieve content in <style> (hurt performance slightly)
+        pre: false // retrieve content in <pre> (hurt performance slightly)
+      });
+
+      let scriptData = "";
+      const scriptTags = root.querySelectorAll("script");
+      for (const scriptTag of scriptTags) {
+        if (
+          typeof scriptTag.rawText.indexOf === "function" &&
+          scriptTag.rawText.indexOf("window._sharedData = ") === 0
+        ) {
+          scriptData = scriptTag.rawText.replace(/;/g, "");
+          scriptData = scriptData.replace(/window._sharedData =/g, "");
+        }
+      }
+
+      const data = JSON.parse(scriptData);
+      userData = data.entry_data.ProfilePage[0].graphql.user;
+      userData.username = userDataCheck.username; // FD: Overwriting the "UsernameChange" with the data from the previous check. The usernames can differ!
+      userData.Error = null;
+    } catch (error) {
+      userData.username = "Not available";
+      userData.full_name = "Not available";
+    }
+
+    return userData;
+  }
 };
 
 start();
---
+++ update-post-metadata.js
@@ -0,0 +1,246 @@
+const axios = require("axios");
+const sleep = require("sleep");
+const moment = require("moment");
+var sqlite3 = require("sqlite3").verbose();
+
+var db = new sqlite3.Database("./data/db.instagram");
+
+// db.on("trace", sql => {
+//   console.log(`Database trace: ${sql}`);
+// });
+
+const start = async function() {
+  console.log("Update metadata posts (existing and new ones)...");
+
+  let userIds = [];
+  let resume = {};
+  db.get(
+    "SELECT Shortcode, Type, EndCursor FROM Resume WHERE Type = 'Metadata' ORDER BY Timestamp DESC LIMIT 1;",
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      } else {
+        resume = results;
+
+        if (resume) {
+          startResumeData();
+        } else {
+          db.all(
+            "SELECT DISTINCT UserId FROM Posts WHERE MetadataUpdate = 0;",
+            (err, results) => {
+              if (err) {
+                console.log(err);
+              } else {
+                // console.log(results);
+                userIds = results;
+
+                getMetadata();
+              }
+            }
+          );
+        }
+      }
+    }
+  );
+
+  async function startResumeData(postInfo) {
+    console.log(
+      `Resuming userid ${resume.Shortcode} for Type ${resume.Type} with End Cursor ${resume.EndCursor}`
+    );
+
+    if (resume.Type === "Metadata") {
+      await getPostData(resume.Shortcode);
+    }
+
+    console.log(`Updating status for post ${postInfo.Shortcode}...`);
+    db.run(
+      `UPDATE Posts SET MetadataUpdate = 1 WHERE Shortcode = "${postInfo.Shortcode}"`
+    );
+
+    console.log(
+      `Resuming done... deleting resume info and start scraping again...`
+    );
+    // db.run(`DELETE FROM Resume WHERE Shortcode = "${postInfo.Shortcode}`);
+
+    db.all(
+      "SELECT DISTINCT UserId FROM Posts WHERE MetadataUpdate = 0;",
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        } else {
+          // console.log(results);
+          userIds = results;
+
+          deleteResumeData(resume.Shortcode, resume.Type, resume.EndCursor);
+          resume = undefined; // FD: For the safety... accessing should result in an error.
+
+          getMetadata();
+        }
+      }
+    );
+  }
+
+  async function createResumeData(shortcode, type, endCursor) {
+    db.run("INSERT INTO Resume (Shortcode, Type, EndCursor) VALUES (?, ?, ?)", [
+      shortcode,
+      type,
+      endCursor
+    ]);
+  }
+
+  async function deleteResumeData(shortcode, type, endCursor) {
+    db.run(
+      `DELETE FROM Resume WHERE Shortcode = "${shortcode}" AND Type = "${type}" AND EndCursor = "${endCursor}"`
+    );
+  }
+
+  async function getMetadata() {
+    console.log(`Found ${userIds.length} user ids...`);
+
+    for (const userId of userIds) {
+      console.log(`Getting data for user id ${userId.UserId}...`);
+
+      await getPostData(userId.UserId);
+    }
+  }
+
+  async function getPostData(userId = -1, hasNextPage = false, endCursor = "") {
+    let postsUrl = "";
+    if (!hasNextPage) {
+      postsUrl = `https://www.instagram.com/graphql/query/?query_hash=f2405b236d85e8296cf30347c9f08c2a&variables={"id":"${userId}","first":50}`;
+    } else {
+      postsUrl = `https://www.instagram.com/graphql/query/?query_hash=f2405b236d85e8296cf30347c9f08c2a&variables={"id":"${userId}","first":50,"after":"${endCursor}"}`;
+    }
+
+    try {
+      await createResumeData(userId, "Metadata", endCursor);
+
+      const response = await axios.get(postsUrl);
+      const edges = response.data.data.user.edge_owner_to_timeline_media.edges;
+      const pageInfo =
+        response.data.data.user.edge_owner_to_timeline_media.page_info;
+
+      for (const post of edges) {
+        const node = post.node;
+
+        console.log(
+          `Getting data for post ${node.shortcode} for user id ${userId}...`
+        );
+
+        const caption = node.edge_media_to_caption; // FD: New approach for the "first comment".
+
+        const postInfo = {
+          shortcode: node.shortcode,
+          userid: node.owner.id,
+          username: node.owner.username,
+          userFullName: "to be filled", // FD: Seems I cannot get the data via the above graphq query.
+          url: `https://www.instagram.com/p/${node.shortcode}`,
+          caption:
+            caption.edges[0] !== undefined ? caption.edges[0].node.text : "",
+          imageUrl: node.thumbnail_src,
+          likesCount: node.edge_media_preview_like.count,
+          timestamp: node.taken_at_timestamp,
+          isVideo: node.is_video,
+          videoViewCount: node.video_view_count ? node.video_view_count : 0,
+          videoUrl: node.video_url ? node.video_url : "",
+          trackingToken: node.tracking_token,
+          takenAt: node.taken_at_timestamp
+        };
+
+        var dateTime = moment(postInfo.timestamp);
+
+        db.get(
+          `SELECT Count(1) as PostExists FROM Posts WHERE Shortcode = '${postInfo.shortcode}';`,
+          (err, post) => {
+            if (err) {
+              console.log(err);
+            } else {
+              if (!post.PostExists) {
+                console.log(
+                  `Inserting new post ${postInfo.shortcode} for user ${userId}...`
+                );
+
+                db.run(
+                  "INSERT INTO Posts (ShortCode, UserId, Username, UserFullName, Url, ImageUrl, Caption, LikesCount, Created, CreatedDate, CreatedTime, HashtagSearch, IsVideo, VideoViewCount, VideoUrl, TrackingToken, TakenAt, Complete, MetadataUpdate, Error, Chunk) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                  [
+                    postInfo.shortcode,
+                    postInfo.userid,
+                    postInfo.username,
+                    postInfo.userFullName,
+                    postInfo.url,
+                    postInfo.imageUrl,
+                    postInfo.caption,
+                    postInfo.likesCount,
+                    postInfo.timestamp,
+                    dateTime.format("YYYY-MM-DD"),
+                    dateTime.format("HH:mm:ss"),
+                    null, // HashtagSearch
+                    postInfo.isVideo,
+                    postInfo.videoViewCount,
+                    postInfo.videoUrl,
+                    postInfo.trackingToken,
+                    postInfo.takenAt,
+                    0,
+                    1,
+                    null,
+                    "J3"
+                  ],
+                  error => {
+                    console.log("insert failed", error);
+                    console.log(
+                      postInfo,
+                      dateTime.format("YYYY-MM-DD"),
+                      dateTime.format("HH:mm:ss")
+                    );
+                  }
+                );
+              } else {
+                console.log(
+                  `Updating existing post ${postInfo.shortcode} for user ${userId}...`
+                );
+                db.run(
+                  `UPDATE Posts SET Username = '${postInfo.username}', 
+                                    UserFullName = '${
+                                      postInfo.userFullName
+                                    }', Caption = '${postInfo.caption}',
+                                    LikesCount = ${
+                                      postInfo.likesCount
+                                    }, CreatedDate = ${dateTime.format(
+                    "YYYY-MM-DD"
+                  )},
+                                    CreatedTime = ${dateTime.format(
+                                      "HH:mm:ss"
+                                    )}, IsVideo = ${postInfo.isVideo},
+                                    VideoViewCount = ${
+                                      postInfo.videoViewCount
+                                    }, VideoUrl = '${
+                    postInfo.videoUrl
+                  }', TrackingToken = '${postInfo.trackingToken}', TakenAt = '${
+                    postInfo.takenAt
+                  }', MetadataUpdate = 1 WHERE Shortcode = '${
+                    postInfo.shortcode
+                  }'`
+                );
+                return;
+              }
+            }
+          }
+        );
+      }
+
+      await deleteResumeData(userId, "Metadata", endCursor);
+
+      if (pageInfo.has_next_page) {
+        await getPostData(userId, true, pageInfo.end_cursor);
+      }
+    } catch (error) {
+      // FD...
+      console.log(error);
+      console.log(`Waiting for 300 secs, due to http error...`);
+      sleep.sleep(300);
+      await getPostData(userId, hasNextPage, endCursor);
+    }
+  }
+};
+
+start();
